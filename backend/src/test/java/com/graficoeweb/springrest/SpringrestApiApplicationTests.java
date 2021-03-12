5edadc3b9778fc2ae522585b45d9b1fdcbfb4531
pesdesigner <pauloeduardo.dg@outlook.com>
2021-03-11 21:48:46 -0300

    Project created

---
+++ backend/src/test/java/com/graficoeweb/springrest/SpringrestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graficoeweb.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
