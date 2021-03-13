c804a232b0f73ad3b9d8178e18723d18ca0710e5
pesdesigner <pauloeduardo.dg@outlook.com>
2021-03-12 23:01:31 -0300

    JpaRepository Client | Client list

--- backend/src/main/java/com/graficoeweb/springrest/api/controller/ClienteController.java
+++ backend/src/main/java/com/graficoeweb/springrest/api/controller/ClienteController.java
@@ -1,30 +1,40 @@
 package com.graficoeweb.springrest.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.graficoeweb.springrest.domain.model.Cliente;
+import com.graficoeweb.springrest.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@GetMapping("/clientes")
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@GetMapping
 	public List<Cliente> listar() {
-		var c1 = new Cliente();
-		c1.setId(1L);
-		c1.setNome("Edu");
-		c1.setTelefone("321245");
-		c1.setEmail("email@teste");
-		
-		var c2 = new Cliente();
-		c2.setId(1L);
-		c2.setNome("Maria");
-		c2.setTelefone("321245");
-		c2.setEmail("email@teste");
-		
-		return Arrays.asList(c1, c2);
+		return clienteRepository.findAll();
 	}
+	
+	@GetMapping("/{clienteId}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	
 }
